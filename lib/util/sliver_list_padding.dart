600e2b11ab98a4d3412ddda76425e649e5021d39
PartyDonut <PartyDonut@users.noreply.github.com>
2025-02-08 08:22:13 +0100

    fix: Increased default padding for item views

--- lib/util/sliver_list_padding.dart
+++ lib/util/sliver_list_padding.dart
@@ -1,14 +1,15 @@
-import 'package:fladder/util/adaptive_layout.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:fladder/models/settings/home_settings_model.dart';
+import 'package:fladder/util/adaptive_layout.dart';
 
 class DefautlSliverBottomPadding extends StatelessWidget {
   const DefautlSliverBottomPadding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return (AdaptiveLayout.of(context).isDesktop || kIsWeb)
-        ? const SliverToBoxAdapter()
+    return (AdaptiveLayout.viewSizeOf(context) != ViewSize.phone)
+        ? SliverPadding(padding: EdgeInsets.only(bottom: 35 + MediaQuery.of(context).padding.bottom))
         : SliverPadding(padding: EdgeInsets.only(bottom: 85 + MediaQuery.of(context).padding.bottom));
   }
 }
@@ -18,7 +19,7 @@ class DefaultSliverTopBadding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return (AdaptiveLayout.of(context).isDesktop || kIsWeb)
+    return (AdaptiveLayout.viewSizeOf(context) != ViewSize.phone)
         ? const SliverPadding(padding: EdgeInsets.only(top: 35))
         : SliverPadding(padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top));
   }
