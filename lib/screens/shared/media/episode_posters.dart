23806270fc0a817bafbfbabb54f00e53b42d061d
PartyDonut <PartyDonut@users.noreply.github.com>
2025-11-07 09:35:54 +0100

    fix: Episode posters placeholder

--- lib/screens/shared/media/episode_posters.dart
+++ lib/screens/shared/media/episode_posters.dart
@@ -154,7 +154,7 @@ class EpisodePoster extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Widget placeHolder = Container(
-      color: Theme.of(context).colorScheme.surfaceContainerHighest,
+      height: double.infinity,
       child: const Icon(Icons.local_movies_outlined),
     );
     bool episodeAvailable = episode.status == EpisodeStatus.available;
